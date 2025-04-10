8cd2fba81e46a35ff476f76df16d30664866ec86
detossj <jorgerubilarssj@gmail.com>
2025-04-10 16:58:34 -0400

    Elements Card added

--- app/src/main/java/com/deto/mediaexplorer/SecondScreen.kt
+++ app/src/main/java/com/deto/mediaexplorer/SecondScreen.kt
@@ -1,18 +1,28 @@
 package com.deto.mediaexplorer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -25,17 +35,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+val categories = getCategories()
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SecondScreen( navController: NavController, category:Int ){
+
+    val SelectedCategory = categories.find { it.id == category }
+
 
     var selected by remember { mutableIntStateOf(0) }
 
@@ -94,7 +111,7 @@ fun SecondScreen( navController: NavController, category:Int ){
                     }
 
                     Button(
-                        onClick = { if( selected == 0 ) {} else navController.navigate(SecondScreen(selected)) },
+                        onClick = {  },
                         shape = CircleShape,
                         contentPadding = PaddingValues(0.dp)
                     ) {
@@ -112,8 +129,76 @@ fun SecondScreen( navController: NavController, category:Int ){
     ) { innerPadding ->
 
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(1),
+                contentPadding = PaddingValues(20.dp),
+                modifier = Modifier.fillMaxWidth()
+
+            ) {
+                items(SelectedCategory?.elements?: emptyList()  ) {
+                    Card(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .clickable { selected = it.id },
+                        colors = CardColors(
+                            contentColor = Color.White,
+                            containerColor =  if (selected == it.id ) Color.Red else Color.Gray ,
+                            disabledContentColor = Color.White,
+                            disabledContainerColor = Color.Transparent
+                        )
+                    ) {
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(20.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            // Columna izquierda: imagen
+                            Column(
+                                modifier = Modifier
+                                    .weight(1f),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Image(
+                                    painter = painterResource(it.Imagen),
+                                    contentDescription = "Image Element",
+                                    modifier = Modifier.size(200.dp)
+                                )
+                            }
+
+                            // Columna derecha: título y descripción
+                            Column(
+                                modifier = Modifier
+                                    .weight(2f)
+                                    .padding(start = 16.dp),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.Start
+                            ) {
+                                Text(
+                                    text = it.title,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = it.description,
+                                    fontSize = 14.sp
+                                )
+                            }
+                        }
+
+                    }
+                }
+            }
 
         }
 
