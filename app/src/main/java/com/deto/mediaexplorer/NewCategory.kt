cae1c848a376e9ad1f869109d3ebe6ee4939f3e6
detossj <jorgerubilarssj@gmail.com>
2025-04-10 20:40:53 -0400

    TopAppBar added to NewCategory.kt

--- app/src/main/java/com/deto/mediaexplorer/NewCategory.kt
+++ app/src/main/java/com/deto/mediaexplorer/NewCategory.kt
@@ -1,9 +1,64 @@
 package com.deto.mediaexplorer
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewCategory( navController: NavController, listCategory: String){
+fun NewCategory( navController: NavController){
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            LargeTopAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp),
+                title = {
+                    Column (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 20.dp)
+                    ) {
+
+                        Text(
+                            text = "New Category",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp
+                        )
+
+                        Text(
+                            text = "Enter the data for the new category",
+                            fontSize = 15.sp
+                        )
+
+                    }
+                }
+
+            )
+        }
+
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
 
 }
