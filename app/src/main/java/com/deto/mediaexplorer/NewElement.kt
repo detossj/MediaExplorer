5e13c3baf96bfa368bd7f9d26567f082371842a7
detossj <jorgerubilarssj@gmail.com>
2025-04-12 16:16:06 -0400

    validation of data and error message added in NewElement.kt

--- app/src/main/java/com/deto/mediaexplorer/NewElement.kt
+++ app/src/main/java/com/deto/mediaexplorer/NewElement.kt
@@ -13,27 +13,41 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewElement(navController: NavController){
 
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    val classification = listOf<Int>(1,2,3,4,5)
+    var selectedClassification by remember { mutableStateOf(classification[0]) }
     var error by remember { mutableStateOf(false) }
+    var error2 by remember { mutableStateOf(false) }
 
     Scaffold(
 
@@ -53,7 +67,13 @@ fun NewElement(navController: NavController){
                     }
 
                     Button(
-                        onClick = {  },
+                        onClick = {
+                            error = title.isEmpty()
+                            error2 = description.isEmpty()
+
+                            // if a la otra screen
+
+                        },
                         shape = CircleShape,
                         contentPadding = PaddingValues(0.dp)
                     ) {
@@ -77,12 +97,21 @@ fun NewElement(navController: NavController){
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight()
-                    .padding(horizontal = 40.dp)
+                    .padding(horizontal = 40.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp),
                     value = title,
+                    leadingIcon = {
+                        Icon(
+                            painterResource(R.drawable.title_24px),
+                            contentDescription = "title",
+                            tint = Color.White
+                        )
+                    },
+                    supportingText = { if(error) Text("Enter the title") else {} },
                     isError = error,
                     onValueChange = { title = it },
                     label = { Text("Title") },
@@ -93,12 +122,63 @@ fun NewElement(navController: NavController){
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp),
                     value = description,
-                    isError = error,
+                    leadingIcon = {
+                        Icon(
+                            painterResource(R.drawable.description_24px),
+                            contentDescription = "title",
+                            tint = Color.White
+                        )
+                    },
+                    supportingText = { if(error2) Text("Enter the description") else {} },
+                    isError = error2,
                     onValueChange = { description = it },
                     label = { Text("Description") },
                     placeholder = { Text("Description") }
 
                 )
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded }
+                ) {
+                    OutlinedTextField(
+                        value = selectedClassification.toString(),
+                        leadingIcon = {
+                            Icon(
+                                painterResource(R.drawable.star_24px),
+                                contentDescription = "title",
+                                tint = Color.White
+                            )
+                        },
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text("Classification") },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded)},
+                        modifier = Modifier
+                            .menuAnchor(type = MenuAnchorType.PrimaryEditable, enabled = true)
+                            .fillMaxWidth()
+                            .padding(vertical = 20.dp),
+
+
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        classification.forEach { item ->
+                            DropdownMenuItem(
+                                text = { Text(item.toString()) },
+                                onClick = {
+                                    selectedClassification = item
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+
+
+                }
 
 
 
